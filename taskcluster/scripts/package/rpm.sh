3121ed0d67343413bb26b17e580887e4303a7fd3
Heitor Neiva <hneiva@mozilla.com>
2022-06-24 11:15:30 -0700

    ci: RPM Packaging task

---
+++ taskcluster/scripts/package/rpm.sh
@@ -0,0 +1,23 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -e
+
+# Output directory for task
+mkdir -p /builds/worker/artifacts/
+
+# Uncompress files
+tar xvf target.tar.gz
+
+# Setup build
+yum-builddep -y mozillavpn.spec
+
+# Build RPM
+rpmbuild -D "_topdir $(pwd)" -D "_sourcedir $(pwd)" -ba mozillavpn.spec
+
+# Copy artifact (exclude debug rpms)
+cp \
+ $(find RPMS/ -name '*.rpm' -not -name '*debug*' | head -n 1) \
+ /builds/worker/artifacts/mozillavpn.rpm
+ 
