e5328f906aa7bbe7ce7310951fd9aa2ed7c80c9e
Gabriel Bustamante <gbustamante@mozilla.com>
2022-08-08 11:25:05 -0500

    clean-up

--- taskcluster/mozillavpn_taskgraph/transforms/beetmover.py
+++ taskcluster/mozillavpn_taskgraph/transforms/beetmover.py
@@ -18,12 +18,8 @@ def add_beetmover_worker_config(config, tasks):
         worker_type = task["worker-type"]
         task_name = task["name"]
         task_label = f"{worker_type}-{task_name}"
-        resolve_keyed_by(
-            task,
-            "bucket",
-            item_name=task_label,
-            **{"level": config.params["level"]},
-        )
+        l3_relpro = config.params["level"] == '3' and config.params["tasks_for"] in task["run-on-tasks-for"]
+        bucket = "release" if l3_relpro else "dep"
         build_id = config.params["moz_build_date"]
         build_type = task["attributes"]["build-type"]
         build_os = os.path.dirname(build_type)
@@ -32,7 +28,7 @@ def add_beetmover_worker_config(config, tasks):
             "pub", app_name, "candidates", app_version, build_id, build_os
         )
         destination_paths = [candidates_path]
-        archive_url = "https://ftp.stage.mozaws.net/" if config.params["level"] != 3 else "https://ftp.mozilla.org/"
+        archive_url = "https://ftp.mozilla.org/" if l3_relpro else "https://ftp.stage.mozaws.net/"
         task_description = f"This {worker_type} task will upload a {build_os} release candidate for v{app_version} to {archive_url}{candidates_path}/"
         branch = config.params["head_ref"]
 
@@ -74,7 +70,7 @@ def add_beetmover_worker_config(config, tasks):
 
         worker = {
             "upstream-artifacts": upstream_artifacts,
-            "bucket": task["bucket"],
+            "bucket": bucket,
             "action": "push-to-candidates",
             "release-properties": {
                 "app-name": app_name,
