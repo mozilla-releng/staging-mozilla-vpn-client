5ea38196a2b4abe44d19d7009c79151d15796ef1
Andrew Halberstadt <ahal@mozilla.com>
2022-08-26 16:22:24 -0400

    ci: move addons-bundle task to the build kind
    It turns out that Chain of Trust has a hardcoded list of kinds that are
    allowed to be part of a release pipeline and fails due to this being an
    'addons' task.
    
    We could update this list, but that would involve a new scriptworker
    release + rollout. It's simpler to move this over to the build kind
    instead.
    
    DO NOT LAND: add a dummy task which uploads a release artifact

--- taskcluster/mozillavpn_taskgraph/parameters.py
+++ taskcluster/mozillavpn_taskgraph/parameters.py
@@ -5,6 +5,7 @@
 import os
 
 from taskgraph.parameters import extend_parameters_schema
+from taskgraph.target_tasks import _target_task
 from voluptuous import All, Any, Range, Required
 
 
@@ -24,9 +25,16 @@ extend_parameters_schema(
 )
 
 
+@_target_task("test")
+def target_tasks_test(full_task_graph, parameters, graph_config):
+    return ["build-addons-bundle", "signing-addons-bundle"]
+
+
 def get_decision_parameters(graph_config, parameters):
     head_tag = parameters["head_tag"]
     parameters["version"] = head_tag[1:] if head_tag else ""
 
     pr_number = os.environ.get("MOZILLAVPN_PULL_REQUEST_NUMBER", None)
     parameters["pull_request_number"] = None if pr_number is None else int(pr_number)
+
+    parameters["target_tasks_method"] = "test"
