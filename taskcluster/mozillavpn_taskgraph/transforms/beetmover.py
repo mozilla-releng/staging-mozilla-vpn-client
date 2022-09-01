daa6b8ffb345d9f0c97801f4744e89c4862ec979
Gabriel Bustamante <gbustamante@mozilla.com>
2022-09-01 18:27:39 -0500

    Addons beetmover

--- taskcluster/mozillavpn_taskgraph/transforms/beetmover.py
+++ taskcluster/mozillavpn_taskgraph/transforms/beetmover.py
@@ -13,36 +13,64 @@ transforms = TransformSequence()
 @transforms.add
 def add_beetmover_worker_config(config, tasks):
     for task in tasks:
-        app_name = "vpn"
         worker_type = task["worker-type"]
         is_relpro = (
             config.params["level"] == "3"
             and config.params["tasks_for"] in task["run-on-tasks-for"]
         )
         bucket = "release" if is_relpro else "dep"
-        build_id = config.params["moz_build_date"]
+        moz_build_date = config.params["moz_build_date"]
         build_type = task["attributes"]["build-type"]
         build_os = os.path.dirname(build_type)
         app_version = config.params["version"]
-        candidates_path = os.path.join(
-            "pub", app_name, "candidates", app_version, build_id, build_os
-        )
+
+        def get_candidates_path():
+            if build_type == "addons/opt":
+                return os.path.join(
+                    "pub", "vpn", "addons", "candidates", moz_build_date
+                )
+            return os.path.join(
+                "pub", "vpn", "candidates", app_version, moz_build_date, build_os
+            )
+
+        candidates_path = get_candidates_path()
         destination_paths = [candidates_path]
         archive_url = (
             "https://ftp.mozilla.org/" if is_relpro else "https://ftp.stage.mozaws.net/"
         )
-        task_description = f"This {worker_type} task will upload a {build_os} release candidate for v{app_version} to {archive_url}{candidates_path}/"
+
+        def get_task_description():
+            if build_type == "addons/opt":
+                return f"This {worker_type} task will upload addon release candidates to {archive_url}{candidates_path}/"
+            return f"This {worker_type} task will upload a {build_os} release candidate for v{app_version} to {archive_url}{candidates_path}/"
+
+        task_description = get_task_description()
         branch = config.params["head_ref"]
+
+        def get_release_artifacts(dep):
+            if build_type == "addons/opt" and dep == "build":
+                return [
+                    artifact
+                    for artifact in task["attributes"]["release-artifacts"]
+                    if artifact["name"].endswith(".rcc")
+                ]
+            if build_type == "addons/opt" and dep == "signing":
+                return [
+                    artifact
+                    for artifact in task["attributes"]["release-artifacts"]
+                    if artifact["name"].endswith(".sig") or artifact["name"].endswith(".json")
+                ]
+            return task["attributes"]["release-artifacts"]
 
         upstream_artifacts = []
         for dep in task["dependencies"]:
             upstream_artifacts.append(
                 {
                     "taskId": {"task-reference": f"<{dep}>"},
-                    "taskType": "scriptworker",
+                    "taskType": dep if dep == "build" else "scriptworker",
                     "paths": [
                         release_artifact["name"]
-                        for release_artifact in task["attributes"]["release-artifacts"]
+                        for release_artifact in get_release_artifacts(dep)
                     ],
                 }
             )
@@ -72,10 +100,10 @@ def add_beetmover_worker_config(config, tasks):
             "bucket": bucket,
             "action": "push-to-candidates",
             "release-properties": {
-                "app-name": app_name,
+                "app-name": "vpn",
                 "app-version": app_version,
                 "branch": branch,
-                "build-id": build_id,
+                "build-id": moz_build_date,
                 "platform": build_type,
             },
             "artifact-map": artifact_map,
