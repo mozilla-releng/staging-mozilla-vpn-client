3573c708f04e40bdfbfc4c4376c720986266e2d2
Gabriel Bustamante <gbustamante@mozilla.com>
2022-09-09 16:26:19 -0500

    create tasks on relpro actions

--- taskcluster/mozillavpn_taskgraph/target_tasks.py
+++ taskcluster/mozillavpn_taskgraph/target_tasks.py
@@ -8,7 +8,8 @@ from taskgraph.target_tasks import _target_task
 @_target_task("promote-addons")
 def target_tasks_promote_addons(full_task_graph, parameters, graph_config):
     def filter(task):
-        return False
+        if task.attributes.get("shipping-phase") == "promote-addons":
+            return True
 
     return [label for label, task in full_task_graph.tasks.items() if filter(task)]
 
@@ -16,7 +17,9 @@ def target_tasks_promote_addons(full_task_graph, parameters, graph_config):
 @_target_task("ship-addons")
 def target_tasks_ship_addons(full_task_graph, parameters, graph_config):
     def filter(task):
-        return False
+        if task.attributes.get("shipping-phase") == "ship-addons":
+            return True
+
 
     return [label for label, task in full_task_graph.tasks.items() if filter(task)]
 
@@ -24,7 +27,9 @@ def target_tasks_ship_addons(full_task_graph, parameters, graph_config):
 @_target_task("promote-client")
 def target_tasks_promote_client(full_task_graph, parameters, graph_config):
     def filter(task):
-        return False
+        if task.attributes.get("shipping-phase") == "promote-client":
+            return True
+
 
     return [label for label, task in full_task_graph.tasks.items() if filter(task)]
 
@@ -32,6 +37,8 @@ def target_tasks_promote_client(full_task_graph, parameters, graph_config):
 @_target_task("ship-client")
 def target_tasks_ship_client(full_task_graph, parameters, graph_config):
     def filter(task):
-        return False
+        if task.attributes.get("shipping-phase") == "ship-client":
+            return True
+
 
     return [label for label, task in full_task_graph.tasks.items() if filter(task)]
