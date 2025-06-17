4313e6b836a9c85fee75d58dbf2b497760cd3bd2
Naomi Kirby <oskirby@gmail.com>
2025-06-17 12:20:32 -0700

    Tweak to test signing on staging-mozilla-vpn-client

--- taskcluster/mozillavpn_taskgraph/transforms/signing.py
+++ taskcluster/mozillavpn_taskgraph/transforms/signing.py
@@ -113,16 +113,16 @@ def add_hardened_sign_config(config, tasks):
                 cfg["entitlements"] = config.params.file_url(cfg["entitlements"])
 
         # Set the top-level provioning profile.
-        task["worker"]["provisioning-profile-config"] = [
-            {
-                "profile_name": "orgmozillamacosFirefoxVPN.provisionprofile",
-                "target_path": "/Contents/embedded.provisionprofile",
-            },
-            {
-                "profile_name": "orgmozillamacosFirefoxVPNloginitem.provisionprofile",
-                "target_path": "/Contents/Library/LoginItems/MozillaVPNLoginItem.app/Contents/embedded.provisionprofile",
-            },
-        ]
+        #task["worker"]["provisioning-profile-config"] = [
+        #    {
+        #        "profile_name": "orgmozillamacosFirefoxVPN.provisionprofile",
+        #        "target_path": "/Contents/embedded.provisionprofile",
+        #    },
+        #    {
+        #        "profile_name": "orgmozillamacosFirefoxVPNloginitem.provisionprofile",
+        #        "target_path": "/Contents/Library/LoginItems/MozillaVPNLoginItem.app/Contents/embedded.provisionprofile",
+        #    },
+        #]
 
         task["worker"]["hardened-sign-config"] = hardened_sign_config
         task["worker"]["mac-behavior"] = "mac_sign_and_pkg_hardened"
